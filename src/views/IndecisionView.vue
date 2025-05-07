f335a5fd9ab6833ee3743530e011c8c8dfc20e88
Victor Hugo Segundo Aguilar <victorhugo.webdeveloper@outlook.com>
2025-05-07 09:59:13 -0600

    [UPDATE] - adding interface for yes/no response

--- src/views/IndecisionView.vue
+++ src/views/IndecisionView.vue
@@ -10,35 +10,16 @@
     <ChatMessages :messages="messages" />
 
     <!-- MessageBox -->
-    <MessageBox />
+    <MessageBox @send-message="onMessage" />
   </div>
 </template>
 
 <script setup lang="ts">
 import ChatMessages from '@/components/chat/ChatMessages.vue';
 import MessageBox from '@/components/chat/MessageBox.vue';
-import type { ChatMessage } from '@/interfaces/chat-message.interface';
-import { ref } from 'vue';
+import { useChat } from '../composables/useChat';
 
-const messages = ref<ChatMessage[]>([
-  {
-    id: new Date().getTime(),
-    message: 'Hola Mundo!!',
-    itsMine: true,
-  },
-  {
-    id: new Date().getTime() + 1,
-    message: 'Quieres tomar un café?',
-    itsMine: true,
-    image: 'https://yesno.wtf/assets/yes/0-c44a7789d54cbdcad867fb7845ff03ae.gif',
-  },
-  {
-    id: new Date().getTime() + 1,
-    message: 'No!!',
-    itsMine: false,
-    image: 'https://yesno.wtf/assets/yes/0-c44a7789d54cbdcad867fb7845ff03ae.gif',
-  },
+const { messages, onMessage } = useChat()
 
-]);
 
 </script>
